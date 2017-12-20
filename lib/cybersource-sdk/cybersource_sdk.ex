9b110920475fb7ea84eeb05ab017588342c15198
speeddragon <speeddragon@gmail.com>
2017-12-20 18:30:27 +0000

    Add simple test, add GenServer

--- lib/cybersource-sdk/cybersource_sdk.ex
+++ lib/cybersource-sdk/cybersource_sdk.ex
@@ -4,6 +4,39 @@ defmodule CyberSourceSDK do
   service (SOAP) of CyberSource.
   """
 
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(CyberSourceSDK.Client, []),
+    ]
+    opts = [strategy: :one_for_one, name: CyberSourceSDK.Supervisor]
+
+    Supervisor.start_link(children, opts)
+  end
+
+  def authorize(price, merchant_reference_code, card_type, encrypted_payment, bill_to \\ [], worker \\ :merchant) do
+    CyberSourceSDK.Client.authorize(price, merchant_reference_code, card_type, encrypted_payment, bill_to, worker)
+  end
+
+  def capture(order_id, request_id, items \\ [], worker \\ :merchant) do
+    CyberSourceSDK.Client.capture(order_id, request_id, items, worker)
+  end
+
+  def refund(order_id, request_id, items \\ [], worker \\ :merchant) do
+    CyberSourceSDK.Client.refund(order_id, request_id, items, worker)
+  end
+
+  def pay_with_android_pay(price, merchant_reference_code, card_type, encrypted_payment, bill_to \\ [], worker \\ :merchant) do
+    CyberSourceSDK.Client.pay_with_android_pay(price, merchant_reference_code, card_type, encrypted_payment, bill_to, worker)
+  end
+
+  def pay_with_apple_pay(price, merchant_reference_code, card_type, encrypted_payment, request_id, bill_to \\ [], worker \\ :merchant) do
+    CyberSourceSDK.Client.pay_with_apple_pay(price, merchant_reference_code, card_type, encrypted_payment, request_id, bill_to, worker)
+  end
+
   @doc """
   Generate BillTo object to replace parameters in request XML
   """
---
+++ test/cybersource_sdk_test.exs
@@ -0,0 +1,19 @@
+defmodule CyberSourceSDKTest do
+	use ExUnit.Case
+
+	test "Test bill_to generated parameters" do
+		expected_parameters = [
+      first_name: "John",
+      last_name: "Doe",
+      street1: "Maryland Street",
+      street2: "34",
+      city: "New York",
+      country: "USA",
+      email: "john@example.com"
+    ]
+
+		parameters = CyberSourceSDK.bill_to("John", "Doe", "Maryland Street", "34", "New York", "USA", "john@example.com")
+
+		assert expected_parameters == parameters
+	end
+end
