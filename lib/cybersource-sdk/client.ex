0fb900f8cdd45a941509ec3fad4b0574a38d6fc2
The Software Engineer Formerly Known as Zen <z@zen.id.au>
2021-05-14 14:45:10 +1000

    logging

--- lib/cybersource-sdk/client.ex
+++ lib/cybersource-sdk/client.ex
@@ -173,12 +173,10 @@ defmodule CyberSourceSDK.Client do
         {:error, reason}
 
       merchant_reference_code_validated ->
-        Logger.info("Merchant ref. code validated")
         merchant_configuration = get_configuration_params(worker)
         if length(merchant_configuration) > 0 do
           replace_params = CyberSourceSDK.Client.get_configuration_params(worker) ++ credit_card ++ bill_to ++ [reference_id: merchant_reference_code_validated]
 
-          Logger.info("running credit_card_create.xml")
           EEx.eval_file(get_template("credit_card_create.xml"), assigns: replace_params) |> call()
         else
           Helper.invalid_merchant_configuration()
@@ -610,7 +608,6 @@ defmodule CyberSourceSDK.Client do
   # Make HTTPS request
   @spec call(String.t()) :: {:ok, map()} | {:error, String.t()} | {:error, :unknown_response}
   defp call(xml_body) do
-    Logger.info("Running XML with body #{inspect(xml_body)}")
     endpoint = Application.get_env(:cybersource_sdk, :endpoint) || System.get_env("CYBERSOURCE_ENDPOINT")
     timeout = Application.get_env(:cybersource_sdk, :timeout, 8000)
 
